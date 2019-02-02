6138bf251a9dc1cd99f4cf138cff3a668280ff8c
Andy Schneider <andyschndr@gmail.com>
2019-02-01 21:41:05 -0700

    Solution to part 2.1
    Makes a nice plot, dang

--- lab3/Laboratory3.m
+++ lab3/Laboratory3.m
@@ -1,5 +1,25 @@
 %Grab the prepared data
-AuthSigs, ImposterSigs, MeanAuth, MeanImposter = GetDatabase();
+[AuthSigs, ImposterSigs, MeanAuth, MeanImposter] = GetDatabase();
+
+mu_a = mean(MeanAuth)
+sigma_a = cov(MeanAuth)
+% Genuine signatures in green
+plotgaus(mu_a, sigma_a, [0 1 0]);
+hold on;
+mu_f = mean(MeanImposter)
+sigma_f = cov(MeanImposter)
+% Imposter signatures in red
+plotgaus(mu_f, sigma_f, [1 0 0]);
+
+% Plot each class data as a set of points in the 2D plane
+plot(MeanAuth(:,1), MeanAuth(:,2), 'go');
+plot(MeanImposter(:,1), MeanImposter(:,2), 'ro');
+xlabel('Pressure');
+ylabel('Velocity');
+title('Class Points and Gaussians');
+
+
+
 
 
 
