785eb8de372880039a7f2a147966afae73f1bcfa
Patrick Settle <patricksettle@hotmail.com>
2019-02-01 19:06:49 -0700

    Fetched and prepared data for analysis

--- lab3/GetDatabase.m
+++ lab3/GetDatabase.m
@@ -1,21 +1,64 @@
 
-function [PatrickSigs,AndySigs,AndyPatrickSigs,PatrickAndySigs] = GetDatabase()
-    PatrickSigs = LoadSignatureSet('PatrickSigs', 30);
-    AndySigs = LoadSignatureSet('AndySigs', 30);
-    AndyPatrickSigs = LoadSignatureSet('AndyPatrickSigs', 30);
-    PatrickAndySigs = LoadSignatureSet('PatrickAndySigs', 30);
+function [AuthSigs,ImposterSigs,MeanAuth,MeanImposter] = GetDatabase()
+    %Load Signatures
+    AuthSigs            = load("AndyAuthSig.mat");
+    AuthSigs            = struct2array(AuthSigs);
+
+    ImposterSigs        = load("ImposterSig.mat");
+    ImposterSigs        = struct2array(ImposterSigs);
+    
+    %Replace time with velocity.
+    AuthSigs = GetVelocity(AuthSigs);
+    ImposterSigs = GetVelocity(ImposterSigs);
+    
+    %Calculate Mean values from data
+    MeanAuth        = GetMeanValues(AuthSigs);
+    MeanImposter    = GetMeanValues(ImposterSigs);
 end
 
-function [loadedSignatures] = LoadSignatureSet(prefix, count)
-    loadedSignatures = struct('xy',[],'y',[],'p',[]);
-    for i = 1 : count
-        filename = strcat(prefix, '/', int2str(i), '.csv');
-        Sig = csvread(filename,1,0);
+function [VelocitySignatures] = GetVelocity(TimeSignatures)
+    % Prepare a velocity return value.   
+    VelocitySignatures = TimeSignatures;
+    
+    SingatureCount = length(TimeSignatures);
+    
+    % Iterate over signatures and calculate velocity
+    for i = 1 : SingatureCount
+        SignatureIn = TimeSignatures(i);
+        SignatureIn = SignatureIn{1};
+        DataCount = length(SignatureIn(1, :));
         
-        loadedSignatures(i).xy = double(Sig(:,1:2));
-        loadedSignatures(i).t = double(Sig(:,4));
-        loadedSignatures(i).p = round(double(Sig(:,3))*255);       
-        loadedSignatures(i).xy(:,2) = max(loadedSignatures(i).xy(:,2)) - loadedSignatures(i).xy(:,2);
+        XVel        = zeros(1, DataCount - 1);
+        YVel        = zeros(1, DataCount - 1);
+        Velocity    = zeros(1, DataCount - 1);
+        
+        X = SignatureIn(1, :);
+        Y = SignatureIn(2, :);
+        Time = SignatureIn(4, :);
+        for j = 2 : DataCount
+            dX = X(j) - X(j - 1);
+            dY = Y(j) - Y(j - 1);
+            dT = Time(j) - Time(j - 1);
+            
+            XVel(j - 1, 1) = dX / dT;
+            YVel(j - 1, 2) = dY / dT;
+            Velocity(j - 1) = sqrt(dX * dX + dY * dY);
+        end
+        %Trim last element of velocity since we can only calculate
+        %DataCount - 1 velocities.
+        VelocitySignatures{i} = VelocitySignatures{i}(:,1:end-1);
+        VelocitySignatures{i}(4,:) = Velocity;
+    end
+end
+
+function [MeanValues] = GetMeanValues(Signatures)
+    %Prepare a return value, col 1 is pressure, col 2 is velocity
+    SignatureCount = length(Signatures);
+    MeanValues = zeros(SignatureCount,2);
+
+    for i = 1 : SignatureCount
+        MeanValues(i,1) = mean(Signatures{i}(3,:));
+        MeanValues(i,2) = mean(Signatures{i}(4,:));
     end
 end
 
--- lab3/Laboratory3.m
+++ lab3/Laboratory3.m
@@ -1,78 +1,8 @@
-% Get the raw signature data
-[PatrickSigs,AndySigs,AndyPatrickSigs,PatrickAndySigs] = GetDatabase();
-
-% Filter out samples that occur at the same time
-count = size(AndySigs);
-count = count(2);
-AndySigsFiltered = struct('coord', {}, 'prs', {}, 'time', {});
-for i = 1:count
-    AndySigsFiltered(i) = FilterTime(AndySigs(i));
-end
-
-% Save all the signature data to single file
-for i=1:count
-    s(i) = AndySigsFiltered(i);
-    sAll_authentic{i} = double(struct2array(s(i))'); % transpose
-end
-save authSig sAll_authentic;
-
-count = size(PatrickAndySigs);
-count = count(2);
-PatrickAndySigsFiltered = struct('coord', {}, 'prs', {}, 'time', {});
-for i = 1:count
-    PatrickAndySigsFiltered(i) = FilterTime(PatrickAndySigs(i));
-end
-
-% Save all the signature data to single file
-for i=1:count
-    s(i) = PatrickAndySigsFiltered(i);
-    sAll_forged{i} = double(struct2array(s(i))'); % transpose
-end
-save forgSig sAll_forged;
-
-% Filter out samples that occur at the same time
-count = size(PatrickSigs);
-count = count(2);
-PatrickSigsFiltered = struct('coord', {}, 'prs', {}, 'time', {});
-for i = 1:count
-    PatrickSigsFiltered(i) = FilterTime(PatrickSigs(i));
-end
-
-% Save all the signature data to single file
-for i=1:count
-    s(i) = PatrickSigsFiltered(i);
-    sAll_imposter{i} = double(struct2array(s(i))'); % transpose
-end
-save imposSig sAll_imposter;
+%Grab the prepared data
+AuthSigs, ImposterSigs, MeanAuth, MeanImposter = GetDatabase();
 
 
 
-function [res] = FilterTime(sig)
-    %initialze a buffer for filtered time, coordinate and pressure values
-    j = 1;
-    t = zeros(size(sig.t,1)-1, 1);
-    xy = zeros(size(sig.t,1)-1, 2);
-    p = zeros(size(sig.t,1)-1, 1);
-    for i = 1:size(sig.t,1)-1
-        %skip any measurements made less than 1ms apart
-        if(sig.t(i+1) - sig.t(i) <= 1.0)
-            continue;
-        end
 
-        t(j) = sig.t(i);
-        xy(j, 1) = sig.xy(i, 1);
-        xy(j, 2) = sig.xy(i, 2);
-        p(j) = sig.p(i);
-        j = j + 1;
-    end
 
-    t = t(1:j,:);
-    xy = xy(1:j,:);
-    p = p(1:j,:);
 
-    res = struct('coord', {}, 'prs', {}, 'time', {});
-    res(1).time = t;
-    res(1).coord = xy;
-    res(1).prs = p;
-    res = res(1);
-end
