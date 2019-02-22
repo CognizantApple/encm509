ad3ca155b5d292c17f62a55624d369025d42142d
Patrick Settle <patricksettle@hotmail.com>
2019-02-21 23:08:12 -0700

    Implemented gabor stuff

--- lab45/Laboratory45.m
+++ lab45/Laboratory45.m
@@ -1,7 +1,9 @@
 %Grab the prepared data
 [AndyThumbs,PatrickThumbs] = GetDatabaseLab45();
 
-Lab4Fingerprint1Auto(PatrickThumbs{22}, "img/22_Default/border_5");
-Lab4Fingerprint1Auto(PatrickThumbs{27}, "img/27_Default/border_5");
+GaborShow(PatrickThumbs{27}, 32, 32, 8, 4, 0, 0.5);
 
+
+%Lab4Fingerprint1Auto(PatrickThumbs{22}, "img/22_Default/border_5");
+%Lab4Fingerprint1Auto(PatrickThumbs{27}, "img/27_Default/border_5");
 
